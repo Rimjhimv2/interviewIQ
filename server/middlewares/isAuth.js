9c703812b71a6a1e4f24a9e714595dd6527ee717
Rimjhimv2 <rimjhimv30@gmail.com>
2026-08-08 17:36:59 +0530

    debug production authentication

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,27 +1,43 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
-
-const isAuth = async (req,res,next) => {
+const isAuth = async (req, res, next) => {
     try {
-        let {token} = req.cookies
+        console.log("COOKIES:", req.cookies);
 
-        if(!token){
-            return res.status(400).json({message:"user does not have a token"})
-        }
-        const verifyToken = jwt.verify(token , process.env.JWT_SECRET)
-        
-        if(!verifyToken){
-            return res.status(400).json({message:"user does not have a valid token"})
-        }
-        req.userId = verifyToken.userId
+        const { token } = req.cookies;
 
-        next()
-   
+        if (!token) {
+            console.log("TOKEN NOT FOUND");
+
+            return res.status(400).json({
+                message: "user does not have a token"
+            });
+        }
+
+        const verifyToken = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        console.log("VERIFIED TOKEN:", verifyToken);
+
+        if (!verifyToken) {
+            return res.status(400).json({
+                message: "user does not have a valid token"
+            });
+        }
+
+        req.userId = verifyToken.userId;
+
+        next();
 
     } catch (error) {
-        return res.status(500).json({message:`isAuth error ${error}`})
-    }
-    
-}
+        console.log("AUTH MIDDLEWARE ERROR:", error);
 
-export default isAuth
+        return res.status(500).json({
+            message: `isAuth error ${error.message}`
+        });
+    }
+};
+
+export default isAuth;
